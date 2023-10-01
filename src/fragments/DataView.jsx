f7481ebb8068d92d312ee3b4a28e4ad8fb70947a
Gnana Prakash <purpleowlcodes@gmail.com>
2023-10-01 20:34:32 +0530

    file renamed from jsx to js

--- src/fragments/DataView.jsx
+++ src/fragments/DataView.jsx
@@ -25,6 +25,7 @@ const DataView = (params) => {
     
 
     const getData = async() => {
+        console.log('use effect called')
         try {
             console.log('getting data')
             var coordinates = await getCordinates();
@@ -37,75 +38,72 @@ const DataView = (params) => {
             console.error("Error:", error);
         } 
     }
+
     useEffect(()=>getData,[])
+    
     if(params.data){
         return (
-            <>
             <div className="weather-data-container">
-                    <div className="temperature-container">
-                        <div className="main-temp-container">
-                        
-                            <img src={`https://openweathermap.org/img/wn/${params.data.weather[0].icon}@2x.png`} className="weather-icon"/>
-                            <p>{params.data.weather[0].description}</p>
-                            <Temperature value={params.data.main.temp} className="main-temp-value"/>
-                            
-                        </div>
-                        
-                        <div className="location-container">
-                            <p className="location-name">{params.data.name}</p>
-                        </div>
-                        <div className="other-temp-container">
-                            <table className="temp-table">
-                                <tbody>
-                                {
-                            Object.keys(get_abbr()).map((key)=>{
-                                if(['feels_like','temp_max','temp_min'].includes(key)){
-                                    return (
-                                        <tr key={key}>
-                                            <td>{get_abbr()[key]}</td>
-                                            <td><Temperature value={params.data.main[key]}/></td>
-                                        </tr>
-                                        )
-                                } 
-                                else if(key=='humidity'){
-                                    return (
-                                    <tr key={key}>
-                                        <td>{get_abbr()[key]}</td>
-                                        <td><Humidity value={params.data.main[key]}/></td>
-                                    </tr>
-                                    )
-                                }
-                                else if(key=="pressure"){
-                                    return (
-                                    <tr key={key}>
-                                        <td>{get_abbr()[key]}</td>
-                                        <td><Pressure value={params.data.main[key]}/></td>
-
-                                    </tr>
-                                    )
-                                }
-                                
-                            })
-                            }
-                                </tbody>
-                            
-                        </table>
-                        </div>
-                        
+                <div className="temperature-container">
+                    <div className="main-temp-container">
+                    
+                        <img src={`https://openweathermap.org/img/wn/${params.data.weather[0].icon}@2x.png`} className="weather-icon"/>
+                        <p>{params.data.weather[0].description}</p>
+                        <Temperature value={params.data.main.temp} className="main-temp-value"/>
                         
                     </div>
                     
-                
-                
+                    <div className="location-container">
+                        <p className="location-name">{params.data.name}</p>
+                    </div>
+                    <div className="other-temp-container">
+                        <table className="temp-table">
+                            <tbody>
+                            {
+                        Object.keys(get_abbr()).map((key)=>{
+                            if(['feels_like','temp_max','temp_min'].includes(key)){
+                                return (
+                                    <tr key={key}>
+                                        <td>{get_abbr()[key]}</td>
+                                        <td><Temperature value={params.data.main[key]}/></td>
+                                    </tr>
+                                    )
+                            } 
+                            else if(key=='humidity'){
+                                return (
+                                <tr key={key}>
+                                    <td>{get_abbr()[key]}</td>
+                                    <td><Humidity value={params.data.main[key]}/></td>
+                                </tr>
+                                )
+                            }
+                            else if(key=="pressure"){
+                                return (
+                                <tr key={key}>
+                                    <td>{get_abbr()[key]}</td>
+                                    <td><Pressure value={params.data.main[key]}/></td>
+
+                                </tr>
+                                )
+                            }
+                            
+                        })
+                        }
+                            </tbody>
+                        
+                    </table>
+                    </div>
+                    
+                    
+                </div>
             </div> 
-            </>
         )
     }
     else{
         return(
-        <div>
-            <p>Location Not allowed/Some Error Occured</p>
-        </div>
+            <div>
+                <p>Location Not allowed/Some Error Occured</p>
+            </div>
         )
     }
     
