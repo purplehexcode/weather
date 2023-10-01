c8b7aa51840ce42ccc930eae747596549bd88353
Gnana Prakash <purpleowlcodes@gmail.com>
2023-10-01 19:22:46 +0530

    Update weather.jsx

--- src/services/weather.jsx
+++ src/services/weather.jsx
@@ -2,6 +2,7 @@ import axios from "axios"
 
 const getCordinates = () => {
     return new Promise((resolve,reject)=>{
+        console.log('getting coordinates')
         if(navigator.geolocation){
             var latitude = 0
             var longitude = 0
@@ -9,7 +10,7 @@ const getCordinates = () => {
             navigator.geolocation.getCurrentPosition(position=>{
                 latitude = position.coords.latitude
                 longitude = position.coords.longitude
-                console.log(latitude,longitude)
+                console.log('coordinates',latitude,longitude)
                 resolve( {
                     latitude,longitude
                 })
@@ -29,6 +30,7 @@ const getWeatherData = ({latitude,longitude}) => {
     const api_key = 'ee101955b2befdfd16e0cf86fbcfc9d1'
     var url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${api_key}`
     const promise = axios.get(url)
+    console.log('getting weather data')
     return promise.then((response)=>{
         return response.data
     })
